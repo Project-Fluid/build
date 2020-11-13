b95801eb41d41ae21042245df3c9e748b3926152
ender1324 <ender1324@abv.bg>
2020-11-13 13:21:56 +0200

    build: Stop building outdated/useless apps

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -22,8 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 
 # /product packages
 PRODUCT_PACKAGES += \
-    Browser2 \
-    Calendar \
     Camera2 \
     Contacts \
     DeskClock \
@@ -31,14 +29,11 @@ PRODUCT_PACKAGES += \
     Music \
     OneTimeInitializer \
     preinstalled-packages-platform-handheld-product.xml \
-    QuickSearchBox \
     SettingsIntelligence \
     frameworks-base-overlays
 
-ifeq ($(FLUID_BUILD),)
 PRODUCT_PACKAGES += \
     LatinIME
-endif
 
 PRODUCT_PACKAGES_DEBUG += \
     frameworks-base-overlays-debug
